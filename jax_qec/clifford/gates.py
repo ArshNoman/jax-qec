9ef0deb471a6a7c17609e978d45b6e8d66a1312e
arsh <arshnoman2270@gmail.com>
2025-12-17 19:50:37 -0500

    utils.py updated with Pauli Gates as JAX numpy classes

--- jax_qec/clifford/gates.py
+++
@@ -1,6 +0,0 @@
-from jax import numpy as jnp
-import jax
-
-def apply(x: str, state: jnp.array):
-    pass
-
--- jax_qec/utils.py
+++ jax_qec/utils.py
@@ -4,3 +4,11 @@ logical_zero = jnp.array([1.0, 0.0])  # |0⟩ -> [1, 0]
 logical_one = jnp.array([0.0, 1.0])  # |1⟩ -> [0, 1]
 plus_state = jnp.array([1.0 / jnp.sqrt(2), 1.0 / jnp.sqrt(2)])  # |+⟩ state -> [1/√2, 1/√2]
 minus_state = jnp.array([1.0 / jnp.sqrt(2), -1.0 / jnp.sqrt(2)])  # |-⟩ state -> [1/√2, -1/√2]
+
+pauli_x = jnp.array([[0,1],[1,0]])
+pauli_z = jnp.array([[1,0],[0,-1]])
+
+PAULI_MAP = {"X": pauli_x, "Z": pauli_z}
+
+def create_gate_stack(gates: str):
+    return jnp.stack([PAULI_MAP[i] for i in gates])
