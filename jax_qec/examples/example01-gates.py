1097ac913980625ba574d140d0b141bbb7282d8b
arsh <arshnoman2270@gmail.com>
2025-12-17 21:34:28 -0500

    Example implementation of gate logic

---
+++ jax_qec/examples/example01-gates.py
@@ -0,0 +1,13 @@
+from jax_qec.clifford import gates
+from jax_qec.clifford.gates import apply
+from jax_qec.utils import *
+
+# Create a qubit that is a logical zero
+qubit = logical_zero
+
+# Create a sequence of Pauli Gates
+PauliGates = gateStack("XXZX")
+
+# Apply them to the inital qubit and print the result
+qubit = apply(qubit, PauliGates)
+print(qubit)
